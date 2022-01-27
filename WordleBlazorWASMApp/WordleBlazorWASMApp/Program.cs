8e04680f65d121e4426e5e57b3260f52e472f1be
John Tomlinson <jtomlinson@brightree.com>
2022-01-27 12:17:56 +0000

    Final changes before merge
    * Copied changes from Blazor server app to Blazor WASM app which ensures that the game logic is within the GameEngine
    * Tidied up the about page and added a link to the GitHub project

--- WordleBlazorWASMApp/WordleBlazorWASMApp/Program.cs
+++ WordleBlazorWASMApp/WordleBlazorWASMApp/Program.cs
@@ -4,9 +4,13 @@ using WordleBlazorWASMApp;
 using WorldleGameEngine;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var possibleWordles = builder.Configuration.GetSection("PossibleWordles").Get<List<string>>();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSingleton(possibleWordles);
 builder.Services.AddSingleton<IGameEngine, GameEngine>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
