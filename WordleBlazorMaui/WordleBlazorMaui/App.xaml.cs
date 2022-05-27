1f4d2c24661468994f70f6d2ec87a6fdb47ae30b
John Tomlinson <jtomlinson@brightree.com>
2022-05-27 16:06:38 +0100

    Created Wordle Blazor Maui app

---
+++ WordleBlazorMaui/WordleBlazorMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WordleBlazorMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ WordleBlazorMaui/WordleBlazorMaui/MauiProgram.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using WorldleGameEngine;
+
+namespace WordleBlazorMaui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+        var a = Assembly.GetExecutingAssembly();
+        using var stream = a.GetManifestResourceStream("WordleBlazorMaui.appsettings.json");
+
+        var config = new ConfigurationBuilder()
+          .AddJsonStream(stream)
+          .Build();
+
+        var possibleWordles = config.GetSection("PossibleWordles").Get<List<string>>();
+
+        builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+        builder.Services.AddSingleton(possibleWordles);
+        builder.Services.AddSingleton<IGameEngine, GameEngine>();
+
+        return builder.Build();
+	}
+}
